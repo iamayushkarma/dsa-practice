aac28c8bd696fde5e6d21cc5ce57d0dd1d89ab6a
Ayush Karma <ayushkarma.dev@gmail.com>
2026-04-18 11:53:55 +0530

    refactor: add question to #567

--- patterns/sliding_window/PermutationString.java
+++ patterns/sliding_window/PermutationString.java
@@ -45,6 +45,9 @@ public class PermutationString {
      * Time Complexity: O(m * 26)
      * Space Complexity: O(1)
      */
+    // Given two strings s1 and s2, return true if s2 contains a permutation of s1,
+    // or false otherwise. In other words, return true if one of s1's permutations
+    // is the substring of s2.
     public static boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
