d78869764534387c2a2a29016bd58b445d325834
Ayush Karma <ayushkarma.dev@gmail.com>
2026-04-29 21:37:09 +0530

    refactor: update code

--- patterns/sliding_window/PermutationString.java
+++ patterns/sliding_window/PermutationString.java
@@ -81,7 +81,7 @@ public class PermutationString {
     public static void main(String[] args) {
 
         String s1 = "ab";
-        String s2 = "eidbaoooooo";
+        String s2 = "eidbaooo";
         System.out.println("Is Permutation in String: " + checkInclusion(s1, s2));
     }
 }
