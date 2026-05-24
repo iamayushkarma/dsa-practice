5fa70fab40721923e01a2c92a472aea30988b3da
Ayush Karma <ayushkarma.dev@gmail.com>
2026-05-24 21:46:13 +0530

    update code

--- patterns/sliding_window/MinimumWindowSubstring.java
+++ patterns/sliding_window/MinimumWindowSubstring.java
@@ -101,7 +101,7 @@ public class MinimumWindowSubstring {
     }
 
     public static void main(String[] args) {
-        String s = "ADOBEEBANDSCABC";
+        String s = "ADOBEEBANDSCAC";
         String t = "ABC";
         System.out.println("Minimum Window Substring: " + minWindow(s, t));
     }
